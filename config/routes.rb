780c6ba12a3bdccb10b4603f1cee3f227b10b266
Sebastian Poblete <sebapobletec@gmail.com>
2019-01-24 00:20:04 -0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products
   get 'pages/about'
+  get 'pages/home'
+  get 'products/index'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
